7f5ba557a04096f0452f6dd7329889901e5454a9
Yuliya Alexandrova <julicherry69@gmail.com>
2023-11-09 15:56:27 +0300

    lesson-3

--- js/helper.js
+++ js/helper.js
@@ -9,3 +9,8 @@ else {
     return false
 }
 };
+
+export const reformatDate = (dateStr) => {
+    const [year, month, day] = dateStr.split('-');
+    return `${day.padStart(2, '0')}.${month.padStart(2, '0')}.${year}`
+};
